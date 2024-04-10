6a3b799c3271d298ddd49553746f5d471f81ed0d
Vietnguyen18 <viet82073@gmail.com>
2024-04-10 19:27:28 +0700

    #2 code base

---
+++ server/middlewares/errHandler.js
@@ -0,0 +1,20 @@
+//su dung de handler loi 
+
+const notFound = (req,res,next) => {
+  const error = new Error(`Route ${req.originalUrl} not found !`);
+  res.status(404);
+  next(error);
+}
+
+const errHandler = (error,req,res,next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    return res.status(statusCode).json({
+        success: false,
+        mes: error?.message,
+    })
+}
+
+module.exports = {
+    notFound,
+    errHandler
+}
--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express =  require('express');
 require('dotenv').config();
-
+const dbConnect = require('./config/dbconnect');
+const initRoutes = require('./routes')
 
 const app = express();
 const port = process.env.PORT || 8888;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); // giup doc duoc data theo kieu array or obj
+dbConnect();
+initRoutes(app)
 
-app.use('/',(rep,res)=>{res.send('SERVER ON')});
+// app.use('/',(rep,res)=>{res.send('SERVER ON')});
 
 app.listen(port,()=>{
     console.log("server running on the port: " + port);
